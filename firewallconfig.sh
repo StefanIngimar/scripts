e24acb3625422a6ebc633ec9b670007e296e78e1
StefanIngimar <stefan.ingimarsson@outlook.com>
2024-05-01 19:06:23 +0200

    allow git

--- firewallconfig.sh
+++ firewallconfig.sh
@@ -9,4 +9,9 @@ sudo ufw default deny incoming
 #allow web browsers
 sudo ufw allow out 80
 sudo ufw allow out 443
+
+# Allow DNS
 sudo ufw allow out 53
+
+# Allow Git over SSH
+sudo ufw allow out 22
